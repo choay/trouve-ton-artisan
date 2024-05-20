9a0ea21e9adbdf6deab5f698f9fb350ebccc1f1c
choayb <maghmouli@gmail.com>
2024-05-20 16:14:59 +0200

    0.10

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,27 +1,29 @@
+'use client'
 import React from 'react';
 
 const ContactForm = () => {
   const handleSubmit = (event) => {
     event.preventDefault();
-    
+    const formData = new FormData(event.target);
+    // Envoyez les données au serveur ici
   };
 
   return (
-    <form 
-     className="max-w-md mx-auto">
+    <form onSubmit={handleSubmit} className="max-w-md mx-auto">
+      <input type="hidden" name="csrf_token" value="0_token_csrf" />
       <div className="mb-4">
         <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-600">Nom</label>
-        <input type="text" id="name" name="name" className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500" />
+        <input type="text" id="name" name="name" required className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500" />
       </div>
       <div className="mb-4">
-        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-600">Objet</label>
-        <input type="email" id="email" name="email" className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500" />
+        <label htmlFor="object" className="block mb-2 text-sm font-medium text-gray-600">Objet</label>
+        <input type="text" id="object" name="object" required className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500" />
       </div>
       <div className="mb-6">
         <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-600">Message</label>
-        <textarea id="message" name="message" rows="4" className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"></textarea>
+        <textarea id="message" name="message" rows="4" required className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"></textarea>
       </div>
-      <button type="submit" className="btn p-2 m-2 rounded-md  focus:outline-none ">
+      <button type="submit" className="btn p-2 m-2 rounded-md focus:outline-none">
         Envoyer
       </button>
     </form>
